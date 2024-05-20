c7a04b674192b4a80c77c6c8fc64b0e8f0be034a
Patrick Peterson <ppeterson609@gmail.com>
2024-05-20 09:10:53 -0400

    updated time

--- develop/script.js
+++ develop/script.js
@@ -1,6 +1,5 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+const currentDay = dayjs()
+$('#currentDay').text(currentDay.format('dddd, MMM D'));
 
 
 
